6ab6889d128798ce727d42b78084e6a7e403e121
Bruno-Carvalho <brunooakdev@gmail.com>
2021-01-13 22:33:01 -0300

    Project created

---
+++ backend/src/main/java/dev/brunocarvalho/boakcatalog/BoakcatalogApplication.java
@@ -0,0 +1,13 @@
+package dev.brunocarvalho.boakcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoakcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoakcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/brunocarvalho/boakcatalog/BoakcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brunocarvalho.boakcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoakcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
