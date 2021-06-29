ae0bc0612539870fccc185a250a4fe506e10a99b
Bruno-Carvalho <brunooakdev@gmail.com>
2021-06-29 11:27:48 -0300

    Upload endpoint

--- backend/src/main/java/dev/brunocarvalho/boakcatalog/BoakcatalogApplication.java
+++ backend/src/main/java/dev/brunocarvalho/boakcatalog/BoakcatalogApplication.java
@@ -1,27 +1,14 @@
 package dev.brunocarvalho.boakcatalog;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import dev.brunocarvalho.boakcatalog.services.S3Service;
-
 @SpringBootApplication
-public class BoakcatalogApplication implements CommandLineRunner{
+public class BoakcatalogApplication{
 	
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoakcatalogApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		s3Service.uploadFile("D:\\OneDrive\\Imagens\\MEMEU\\goku.jpg");
-		
-	}
-
 	
 }
